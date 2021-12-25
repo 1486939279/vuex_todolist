1d34856c34cf696c9c9bf2ec84af14c8cfb87439
月生子 <9569006+monthly-child@user.noreply.gitee.com>
2021-12-25 23:36:57 +0800

    vuextodo

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,69 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    inputValue: '',
+    nextId: 5,
+    viewKey: ''
   },
   mutations: {
+    initlist(state, list) {
+      state.list = list
+    },
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    removeItem(state, id) {
+      // 根据id查找对应项的索引
+      const i = state.list.findIndex(x => x.id === id)
+      // 根据索引删除对应的元素
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    changeStatus(state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if (i !== -1) {
+        state.list[i].done = param.status
+      }
+    },
+    cleanDone(state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    changeViewKey(state, key) {
+      state.viewKey = key
+    }
   },
   actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data }) => {
+        context.commit('initlist', data)
+      })
+    }
   },
-  modules: {
-  }
+  getters: {
+    unDoneLength(state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    infoList(state) {
+      if (state.viewKey === 'all') return state.list
+      if (state.viewKey === 'undone') return state.list.filter(x => x.done === false)
+      if (state.viewKey === 'done') return state.list.filter(x => x.done === true)
+    }
+  },
+  modules: {}
 })
